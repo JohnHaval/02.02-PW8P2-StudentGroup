55ca87df1a6bf18b0846552e2c6f2d273007e6e1
Hapro Bishop <acfowo45@gmail.com>
2022-10-30 21:06:11 +0300

    First Add

---
+++ PW8P2-StudentGroup/MainWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PW8P2_StudentGroup
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+
+
+        //Сортировка по 
+        //_allStatInfo.Sort(new Comparison<StatInfo>((x, y) => DateTime.Compare(x.date, y.date)));
+
+
+
+
+
+
+
+
+
+
+        List<Student> Students = new List<Student>();
+        private void StudentsMainInfo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                var student = (Student)e.AddedItems[0];
+                YearsAndMarks.Items.Clear();
+                foreach (var value in student.Subjects)
+                {
+                    YearsAndMarks.Items.Add($"{value.Year} | {value.Name} | {value.Mark}");
+                }
+            }
+            catch { }
+        }
+
+        private void AutoFill_Click(object sender, RoutedEventArgs e)
+        {
+            Students = new List<Student>()
+            { 
+                new Student("Кронов", Convert.ToDateTime("19.02.2022"), 2000, 4, "П-41", AutoFillYearsAndMarks()),
+                new Student("Плутонов", Convert.ToDateTime("19.02.2022"), 2000, 4, "П-41", AutoFillYearsAndMarks()),
+                new Student("Ножнов", Convert.ToDateTime("19.02.2022"), 2000, 4, "П-41", AutoFillYearsAndMarks()),
+                new Student("Криклиев", Convert.ToDateTime("19.02.2022"), 2000, 4, "П-41", AutoFillYearsAndMarks()),
+                new Student("Потеркин", Convert.ToDateTime("19.02.2022"), 2000, 4, "П-41", AutoFillYearsAndMarks()),
+                new Student("Струганов", Convert.ToDateTime("19.02.2022"), 2000, 4, "П-41", AutoFillYearsAndMarks()),
+                new Student("Смирнов", Convert.ToDateTime("19.02.2022"), 2000, 4, "П-41", AutoFillYearsAndMarks()),
+                new Student("Косов", Convert.ToDateTime("19.02.2022"), 2000, 4, "П-41", AutoFillYearsAndMarks()),
+                new Student("Иноров", Convert.ToDateTime("19.02.2022"), 2000, 4, "П-41", AutoFillYearsAndMarks()),
+                new Student("Невоськин", Convert.ToDateTime("19.02.2022"), 2000, 4, "П-41", AutoFillYearsAndMarks()),
+                new Student("Петров", Convert.ToDateTime("19.02.2022"), 2000, 4, "П-41", AutoFillYearsAndMarks()),
+                new Student("Иванов", Convert.ToDateTime("19.02.2022"), 2000, 4, "П-41", AutoFillYearsAndMarks()),
+                new Student("Колисеев", Convert.ToDateTime("19.02.2022"), 2000, 4, "П-41", AutoFillYearsAndMarks()),
+                new Student("Локоткин", Convert.ToDateTime("19.02.2022"), 2000, 4, "П-41", AutoFillYearsAndMarks()),
+                new Student("Прямостронов", Convert.ToDateTime("19.02.2022"), 2000, 4, "П-41", AutoFillYearsAndMarks()),
+                //15, need 25
+                new Student("Сверлов", Convert.ToDateTime("19.02.2022"), 2000, 4, "П-41", AutoFillYearsAndMarks()),
+                new Student("Скосов", Convert.ToDateTime("19.02.2022"), 2000, 4, "П-41", AutoFillYearsAndMarks()),
+                new Student("Ильев", Convert.ToDateTime("19.02.2022"), 2000, 4, "П-41", AutoFillYearsAndMarks()),
+                new Student("Касаткин", Convert.ToDateTime("19.02.2022"), 2000, 4, "П-41", AutoFillYearsAndMarks()),
+                new Student("Авоськин", Convert.ToDateTime("19.02.2022"), 2000, 4, "П-41", AutoFillYearsAndMarks()),
+                new Student("Попугаев", Convert.ToDateTime("19.02.2022"), 2000, 4, "П-41", AutoFillYearsAndMarks()),
+                new Student("Неонов", Convert.ToDateTime("19.02.2022"), 2000, 4, "П-41", AutoFillYearsAndMarks()),
+                new Student("Локомотов", Convert.ToDateTime("19.02.2022"), 2000, 4, "П-41", AutoFillYearsAndMarks()),
+                new Student("Прямолинейнов", Convert.ToDateTime("19.02.2022"), 2000, 4, "П-41", AutoFillYearsAndMarks()),
+                new Student("Состряпов", Convert.ToDateTime("19.02.2022"), 2000, 4, "П-41", AutoFillYearsAndMarks())
+            };
+            StudentsMainInfo.ItemsSource = Students;
+        }
+        private List<Subject> AutoFillYearsAndMarks()
+        {
+            var subjects = new List<Subject>();
+            for (int i = 0; i < 3; i++)
+            {
+                var subject1 = new Subject(Student.IDCounter, "СП", 2000 + i, 4);
+                var subject2 = new Subject(Student.IDCounter, "РПМ", 2000 + i, 5);
+                var subject3 = new Subject(Student.IDCounter, "КС", 2000 + i, 4);
+                var subject4 = new Subject(Student.IDCounter, "ВиПКС", 2000 + i, 4);
+                var subject5 = new Subject(Student.IDCounter, "ТПМ", 2000 + i, 5);
+                var subject6 = new Subject(Student.IDCounter, "ДПП", 2000 + i, 3);
+                subjects.Add(subject1);
+                subjects.Add(subject2);
+                subjects.Add(subject3);
+                subjects.Add(subject4);
+                subjects.Add(subject5);
+                subjects.Add(subject6);
+            }
+            return subjects;
+        }
+    }
+}
---
+++ PW8P2-StudentGroup/Student.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PW8P2_StudentGroup
+{
+    public class Student
+    {
+        public static int IDCounter { get; private set; } = 1;
+        public int StudentID { get; private set; }
+        public string SecondName { get; set; }
+        public DateTime BornDate { get; set; }
+        public int EnterYear { get; set; }
+        public int Course { get; set; }
+        public string Group { get; set; }        
+        public List<Subject> Subjects;
+
+        public Student()
+        {
+            StudentID = IDCounter++;
+            Subjects = new List<Subject>();
+        }
+        public Student(string secondName)
+        {
+            StudentID = IDCounter++;
+            Subjects = new List<Subject>();
+            SecondName = secondName;
+        }
+        public Student(string secondName, DateTime bornDate)
+        {
+            StudentID = IDCounter++;
+            Subjects = new List<Subject>();
+            SecondName = secondName;
+            BornDate = bornDate;
+        }
+        public Student(string secondName, DateTime bornDate, int enterYear)
+        {
+            StudentID = IDCounter++;
+            Subjects = new List<Subject>();
+            SecondName = secondName;
+            BornDate = bornDate;
+            EnterYear = enterYear;
+        }
+        public Student(string secondName, DateTime bornDate, int enterYear, int course)
+        {
+            StudentID = IDCounter++;
+            Subjects = new List<Subject>();
+            SecondName = secondName;
+            BornDate = bornDate;
+            EnterYear = enterYear;
+            Course = course;
+        }
+        public Student(string secondName, DateTime bornDate, int enterYear, int course, string group)
+        {
+            StudentID = IDCounter++;
+            Subjects = new List<Subject>();
+            SecondName = secondName;
+            BornDate = bornDate;
+            EnterYear = enterYear;
+            Course = course;
+            Group = group;
+        }
+        public Student(string secondName, DateTime bornDate, int enterYear, int course, string group, List<Subject> subjects)
+        {
+            StudentID = IDCounter++;
+            Subjects = new List<Subject>();
+            SecondName = secondName;
+            BornDate = bornDate;
+            EnterYear = enterYear;
+            Course = course;
+            Group = group;
+            TransferYearMarks(subjects, false);
+        }
+        public void TransferYearMarks(List<Subject> subjects, bool isNew)
+        {
+            if (isNew)
+            {
+                CreateYearMarks(subjects);
+            }
+            else UpdateYearMarks(subjects);
+        }
+        private void CreateYearMarks(List<Subject> subjects)
+        {
+            Subjects = new List<Subject>();
+            UpdateYearMarks(subjects);
+        }
+        private void UpdateYearMarks(List<Subject> subjects)
+        {
+            foreach (var value in subjects)
+            {
+                YearComplete(value);
+            }
+        }
+        public void YearComplete(Subject subject)
+        {
+            Subjects.Add(subject);
+        }        
+        public int GetAvgMark()
+        {
+            int allMarks = 0;
+            for (int i = 0; i < Subjects.Count; i++)
+            {
+                allMarks += Subjects[i].Mark;
+            }
+            return allMarks / Subjects.Count;
+        }
+    }
+}
